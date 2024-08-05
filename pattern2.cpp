d9206b7ede1baa6c4db56526cdd673eda1448382
GodMode-OFF <79676658+GodMode-OFF@users.noreply.github.com>
2024-08-05 22:57:17 +0530

    Create pattern2.cpp

---
+++ pattern2.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    for(int i = 5; i>=1; i--){
+        for(int j = 5; j>=1; j--){
+            cout<<j;
+        }
+        cout<<"\n";
+    }
+}
