43a72b0ca62e75cd1691df90568b044e51d477c3
GodMode-OFF <79676658+GodMode-OFF@users.noreply.github.com>
2024-08-05 22:50:13 +0530

    Create Pattern1.cpp

---
+++ Pattern1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int q = 0;
+
+
+int main() {
+    for(int i = 1; i<=5; i++){
+        for(int j = 1; j<=5; j++){
+            cout<<q;
+        }
+        q++;
+        cout<<"\n";
+    }
+}
