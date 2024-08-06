bbd6e32c49db6b6cfd212668a0c934e4ee633d60
GodMode-OFF <79676658+GodMode-OFF@users.noreply.github.com>
2024-08-06 18:59:21 +0530

    Create patter6.cpp

---
+++ patter6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=5; i++){
+        char mind = 'a' + i-1;
+        for(int j=1; j<=i; j++){
+            
+            cout<<mind;
+        }
+    cout<<"\n";
+        
+    }
+
+    return 0;
+}
