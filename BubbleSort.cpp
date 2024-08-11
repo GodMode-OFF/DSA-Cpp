961052b7120c05b21b07e35af69d11baf4c7b126
GodMode-OFF <79676658+GodMode-OFF@users.noreply.github.com>
2024-08-12 00:34:04 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 
 int main(){
-    int arr[6] = {1,5,2,4,6,6};
+    int arr[6] = {5,4,3,2,1,6};
     for(int i = 0; i < 6 - 1; i++){
-            if(arr[i]>arr[i+1]){
+        for(int j = 0; j<5; j++){
+            if(arr[j]>arr[j+1]){
                 int secx;
                 secx = arr[j];
-                arr[j] = arr[i];
-                arr[i] = secx;
-                
-            }
+                arr[j] = arr[j+1];
+                arr[j+1] = secx;
+            }}}
+    cout << "Array elements: ";
+    for(int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
     }
+    cout << endl;
+
 }
