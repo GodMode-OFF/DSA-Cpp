cd98e39756fac8986261701bb1f4e7b3a4473bb1
GodMode-OFF <79676658+GodMode-OFF@users.noreply.github.com>
2024-09-01 23:48:10 +0530

    Update KMPalgo.cpp

--- KMPalgo.cpp
+++ KMPalgo.cpp
@@ -1,2 +1,23 @@
 //KMP Algo
 //still dk how to solve this
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+    vector<int>lps(s.size(),0);
+    int pre = 0, suf = 1;
+    
+    
+    while(suf < s.size()){
+        if(s[suf] == s[pre]){
+            lps[suf] = pre + 1;
+        }
+        
+    }
+    
+    
+    return 0;
+}
