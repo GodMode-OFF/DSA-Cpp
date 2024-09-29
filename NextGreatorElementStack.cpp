33105074714708f445df0403cfb32517540c0242
GodMode-OFF <79676658+GodMode-OFF@users.noreply.github.com>
2024-09-29 21:52:27 +0530

    Create NextGreatorElementStack.cpp

---
+++ NextGreatorElementStack.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr(5, -1);
+    vector<int> data = {1, 3, 2, 4, 5};
+    stack<int> s;
+    
+    for(int i = 0; i < 5; i++){
+        while(!s.empty() && data[s.top()] < data[i]){
+            arr[s.top()] = data[i];
+            s.pop();
+        }
+        s.push(i);
+    }
+
+    cout << arr[1] << endl;
+    return 0;
+}
